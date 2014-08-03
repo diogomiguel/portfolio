10867a98b64f72dd73f289bdba8d16946413516e
Diogo Silva <dsilva@dsilva-M860TU.(none)>
2014-08-03 13:08:36 +0100

    First commit

---
+++ js/config.js
@@ -0,0 +1,35 @@
+/**
+ * RequireJS configuration
+ */
+require.config({
+
+	// Initialize the application with the main application file
+	deps: ['plugins/console', 'plugins/istouch', 'main'],
+
+	paths: {
+		'jquery'	: '../components/jquery/jquery.min',
+		'isotope'	: '../components/isotope/jquery.isotope',
+		'imagesLoaded' :  '../components/imagesloaded/imagesloaded.pkgd',
+		'lazyload'	: '../components/jquery.lazyload/jquery.lazyload.min',
+		'simpleStateManager'	: '../components/SimpleStateManager/src/ssm'
+	},
+
+	shim: {
+		// If you need to shim anything, put it here
+		'imagesLoaded' : {
+			deps: ['jquery'],
+			exports: 'imagesLoaded'
+		},
+		'isotope' : {
+			deps: ['jquery'],
+			exports: 'Isotope'
+		},
+		'lazyload' : {
+			deps: ['jquery']
+		},
+		'simpleStateManager': {
+			exports: 'ssm'
+		}
+	}
+
+});
---
+++ js/main.js
@@ -0,0 +1,20 @@
+	/**
+ * Module Description
+ *
+ * @author Author Name
+ * @date 2013-01-01
+ */
+
+require([
+	// Require the modules
+	//'modules/video',
+	
+], function () {
+	'use strict';
+
+
+	// Set JWPlayer key
+	//window.jwplayer.key = "fu2u0kgoWvXIFOm4Iiq7xsU3LJRLkDIfHE8ZKQ==";
+
+	
+});

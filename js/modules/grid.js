23f0598300277e2c8c0854c941b8e458751fc76d
Diogo Silva <diogo.miguel.fonseca@gmail.com>
2014-08-07 19:37:27 +0100

    Details view for blog and abouts. Active state images. CSS improvs details view

--- js/modules/grid.js
+++ js/modules/grid.js
@@ -77,24 +77,41 @@ define(['jquery', 'isotope', 'imagesLoaded'], function ($) {
 			$detailsView =  $("#js-detailsview");
 
 			// Open details view
-			$(".js__grid__item__wrapper__rollover, .js-grid-readmore").on("click", "a", function(e){
+			$(".js__grid__item__wrapper__rollover, .js-grid-readmore").on("click", function(e){
 
-				var $this = $(this);
+				var $this = $(this).children('a');
+
+				// Find grid item parent and make it active after disabling already active ones
+				$grid.children('.grid__item--active').removeClass('grid__item--active');
+				$this.parents('.grid__item').addClass('grid__item--active');
+
 				// Scroll to top
 				$("html, body").animate({ scrollTop: "0" });
 
-				// Prevents double loading
+
+				// Show loader
+				that.showLoader();
 
 				var dataId = $this.attr("data-id"),
 					dataType = $this.attr("data-type"),
 					dataOpenFunc = function() {
 						// Loading screen class
 						$detailsView.removeClass("detailsview--close");
+
+						console.log("Loading: " + dataType);
 						// Get data from JSON after 0.5s
 						switch(dataType) {
 							// Projects
 							case "project":
 								that.loadProject(dataId);
+								break;
+							// About items
+							case "about":
+								that.loadAbout(dataId);
+								break;
+							// Blog items
+							case "blog":
+								that.loadBlog(dataId);
 								break;
 						}
 						detailsViewOpen = true;
@@ -120,10 +137,12 @@ define(['jquery', 'isotope', 'imagesLoaded'], function ($) {
 			$("#js-detailsview-closebtn").on("click", function(){
 				
 				$detailsView.addClass("detailsview--close");
-				
+				$grid.children('.grid__item--active').removeClass('grid__item--active');
+
 				// Remove the other classes after animation finishes
 				setTimeout(function(){
 					$detailsView.removeClass("detailsview--open");
+
 				}, 1200);
 			});
 
@@ -160,17 +179,27 @@ define(['jquery', 'isotope', 'imagesLoaded'], function ($) {
 		},
 
 		loadProject: function(dataId) {
+			var that = this;
+
 			// Load projects JSON
 			$.getJSON("data/projects.json", function(data){
 				var projectData = data[dataId];
 
+				// Remove picture if exists
+				$('#js-detailsview-figure').remove();
+
 
 				// Replace main image
 				if (projectData.imagepath) {
-					var $projectImg = $("#js-detailsview-figure img");
+					var $projectImg = $("<img/>").attr('src', 'dist/img/blank-big.gif'),
+						$projectFigure = $("<figure/>").attr('id', 'js-detailsview-figure').addClass('detailsview__wrapper__figure');
 
 					$projectImg.attr("src", 'dist/img/' + projectData.imagepath)
 					.attr("alt", projectData.title);
+
+					$projectFigure.append($projectImg);
+
+					$('#js-detailsview-wrapper').prepend($projectFigure);
 				}
 
 				// Place header text
@@ -181,46 +210,155 @@ define(['jquery', 'isotope', 'imagesLoaded'], function ($) {
 				$detailsviewDescription.html(projectData.description);
 
 				// Place content technologies
-				var n = 0,
-					$ul = $("<ul>").addClass("detailsview__wrapper__technologies");
-
-				$detailsviewDescription.append("<hr /><h3>Technologies</h3>");
-				
-
-				for(; n < projectData.technologies.length; n++) {
-					var $li = $("<li/>")
-						.text(projectData.technologies[n]);
-					$ul.append($li);
-				}
-
-				$detailsviewDescription.append($ul)
-				.append("<div class='clearfix'></div>");
-
+				that._createDetailsList("Technologies", "detailsview__wrapper__technologies", projectData.technologies, $detailsviewDescription);
 
 				// Place content links
-				n = 0;
-				$ul = $("<ul>").addClass("detailsview__wrapper__links");
-
-				for(; n < projectData.links.length; n++) {
-					var $li2 = $("<li/>"),
-						$a = $('<a/>');
-
-					$a.addClass("detailsview__wrapper__links__link")
-					.text(projectData.links[n][0])
-					.attr("href", projectData.links[n][1]);
-
-					$ul.append($li2);
-					$li2.append($a);
-				}
-				$detailsviewDescription.append("<hr />")
-				.append($ul)
-				.append("<div class='clearfix'></div>");
+				that._createLinksList(projectData.links, $detailsviewDescription);
 
 				// Remove loading state and force open and visible
 				$detailsView.addClass("detailsview--open");
 
 				console.log(projectData);
+
+				that.hideLoader();
 			});
+		},
+
+		loadAbout: function(dataId) {
+			var that = this;
+
+			
+
+			// Load about JSON
+			$.getJSON("data/about.json", function(data){
+
+				var aboutData = data[dataId];
+
+				// Remove picture if exists
+				$('#js-detailsview-figure').remove();
+
+				// Place header text
+				$("#js-detailsview-header").html(aboutData.title);
+
+				// Place content description
+				var $detailsviewDescription = $("#js-detailsview-description");
+				$detailsviewDescription.html(aboutData.description);
+
+				// Place content accordingly to section
+				switch(dataId) {
+					case 'about':
+						// Workplaces list
+						that._createDetailsList("Workplaces", "detailsview__wrapper__workplaces", aboutData.workplaces, $detailsviewDescription);
+
+						// Interests list
+						that._createDetailsList("Other Interests", "detailsview__wrapper__interests", aboutData.interests, $detailsviewDescription);
+
+						// CV
+						that._createLinksList(aboutData.cv, $detailsviewDescription);
+
+						break;
+					case 'contact':
+						// Contacts list
+						that._createDetailsList("Contact Details", "detailsview__wrapper__contacts", aboutData.contacts, $detailsviewDescription);
+						break;
+					case 'skills':
+						// BackEnd list
+						that._createDetailsList("Back End Skills", "detailsview__wrapper__backend", aboutData.backend, $detailsviewDescription);
+
+						// FrontEnd list
+						that._createDetailsList("Front End Skills", "detailsview__wrapper__frontend", aboutData.frontend, $detailsviewDescription);
+
+						// Tools list
+						that._createDetailsList("Productivity Tools", "detailsview__wrapper__tools", aboutData.tools, $detailsviewDescription);
+						break;
+				}
+					
+
+				// Remove loading state and force open and visible
+				$detailsView.addClass("detailsview--open");
+
+				console.log(aboutData);
+
+				that.hideLoader();
+			});
+		},
+
+
+		loadBlog: function(dataId) {
+			var that = this;
+
+			
+
+			// Load blog JSON
+			$.getJSON("data/blog.json", function(data){
+
+				var blogData = data[dataId];
+
+				// Remove picture if exists
+				$('#js-detailsview-figure').remove();
+
+				// Place header text
+				$("#js-detailsview-header").html(blogData.title);
+
+				// Place content description
+				var $detailsviewDescription = $("#js-detailsview-description");
+				$detailsviewDescription.html(blogData.description);
+
+				// CV
+				that._createLinksList(blogData.links, $detailsviewDescription);
+					
+
+				// Remove loading state and force open and visible
+				$detailsView.addClass("detailsview--open");
+
+				console.log(blogData);
+
+				that.hideLoader();
+			});
+		},
+
+		showLoader: function() {
+			$('#js-loading-bg').addClass('loading__bg--active');
+		},
+
+		hideLoader: function() {
+			$('#js-loading-bg').removeClass('loading__bg--active');
+		},
+
+		_createDetailsList: function(title, ulClass, dataObj, $parentEl) {
+			$parentEl.append("<hr /><h3>" + title + "</h3>");
+			var $ul = $("<ul/>").addClass(ulClass),
+				$div = $("<div/>").addClass('detailsview__wrapper__fakeclearcontainer'),
+				n = 0;
+
+			for(; n < dataObj.length; n++) {
+				var $li = $("<li/>")
+				.html(dataObj[n]);
+				$ul.append($li);
+			}
+			$div.append($ul);
+			$parentEl.append($div);
+		},
+
+		_createLinksList: function(dataObj, $parentEl) {
+			var n = 0,
+				$ul = $("<ul>").addClass("detailsview__wrapper__links");
+
+			for(; n < dataObj.length; n++) {
+				var $li = $("<li/>"),
+					$a = $('<a/>');
+
+				$a.addClass("detailsview__wrapper__links__link")
+				.text(dataObj[n][0])
+				.attr("href", dataObj[n][1]);
+
+				$ul.append($li);
+				$li.append($a);
+			}
+
+			$parentEl.append("<hr />")
+			.append($ul)
+			.append("<div class='clearfix'></div>");
 		},
 
 		_applyFiltering: function(criterion) {
